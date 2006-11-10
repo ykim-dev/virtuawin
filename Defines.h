ff13f4e7e58ea30a3b633f138637b7952a74c305
bjasspa <>
2006-11-10 21:33:49 +0000

    Increased MAXWIN to 128

--- Defines.h
+++ Defines.h
@@ -27,24 +27,24 @@
 // application name and version defines
 #define vwVIRTUAWIN_NAME    "VirtuaWin"
 #define vwVIRTUAWIN_EMAIL   "VirtuaWin@home.se"
-#define vwVIRTUAWIN_NAME_VERSION "VirtuaWin v3.0 test 1"
+#define vwVIRTUAWIN_NAME_VERSION "VirtuaWin v3.0 test 2"
 
 // Various defines used on several places 
-#define MAXWIN 100                // max number of windows to handle
-#define MAXDESK 11                // max number of desktops (0 - not used, 1 - 9 - normal, 10 private) 
-#define MAXUSER 10                // max number of user windows to search for
-#define UWM_SYSTRAY (WM_USER + 1) // Sent to us by the systray
-#define MAXMODULES 10             // max number of modules to handle
+#define MAXWIN          128       // max number of windows to handle
+#define MAXDESK          11       // max number of desktops (0 - not used, 1 - 9 - normal, 10 private) 
+#define MAXUSER          10       // max number of user windows to search for
+#define MAXMODULES       10       // max number of modules to handle
 #define vwCLASSNAME_MAX  47       // maximum length of a class name (buffer needs to be n+1 long)
 #define vwMODULENAME_MAX 79       // Maximum length of a module name (buffer needs to be n+1 long)
 #define vwDESK_PRIVATE1  10       // Deesk number of the first private desk
+#define UWM_SYSTRAY (WM_USER + 1) // Sent to us by the systray
 
 // Internal messages for mouse controlling
-#define VW_MOUSEWARP  WM_USER + 90
-#define VW_MOUSEUP    WM_USER + 91
-#define VW_MOUSEDOWN  WM_USER + 92
-#define VW_MOUSELEFT  WM_USER + 93
-#define VW_MOUSERIGHT WM_USER + 94
-#define VW_MOUSERESET WM_USER + 95
+#define VW_MOUSEWARP   WM_USER + 90
+#define VW_MOUSEUP     WM_USER + 91
+#define VW_MOUSEDOWN   WM_USER + 92
+#define VW_MOUSELEFT   WM_USER + 93
+#define VW_MOUSERIGHT  WM_USER + 94
+#define VW_MOUSERESET  WM_USER + 95
 
 #endif
