ad0e388ef8f8ee00169e952ab4b7e1f94ea130cb
bjasspa <>
2007-05-01 23:06:56 +0000

    Changed script to (ba)sh as I don't have tcsh Enhanced it to build unicode version of VW and installer Updated source package file list.

--- scripts/createPackage.sh
+++ scripts/createPackage.sh
@@ -1,134 +1,176 @@
-#!tcsh
 #
 # Script for automating the build and release process as much as possible.
 #
 # Preconditions:
-# - A file called .cvssettings in your home that sets the needed stuff for CVS access
-# - A helpfile compiler
-# - gcc with MinGW 
-# - Inno Setup
-# - emacs (or another good text editor)
-# - ncftp
-# - winzip with commandline extension
-# - cvs 
+# - shell already configured to run gcc with MinGW and CVS (i.e. CVSROOT and CVS_RSH set)
+# - A helpfile compiler (set HELPCOMPILER)
+# - Inno Setup (set SETUPCOMPILER if not in standard install location)
+# - emacs (or set EDITOR to another good text editor)
+# - zip (set ZIP) or winzip with commandline extension
+# - ncftp (if uploading to SF)
 #
 # Variables
-alias EDITOR        emacs
-alias HELPCOMPILER  "/cygdrive/c/helpcompiler/HCW /C /E"
-alias SETUPCOMPILER "/cygdrive/c/Program\ Files/Inno\ Setup\ 5/Compil32 /cc"
-alias WINZIP        "/cygdrive/c/Program\ Files/WinZip/wzzip"
+if [ -z "$EDITOR" ] ; then
+    EDITOR=emacs
+fi
+if [ -z "$HELPCOMPILER" ] ; then
+    HELPCOMPILER="/cygdrive/c/helpcompiler/HCW"
+fi
+if [ -z "$SETUPCOMPILER" ] ; then
+    SETUPCOMPILER="/cygdrive/c/Program Files/Inno Setup 5/Compil32"
+fi
+if [ -z "$WINZIP" ] ; then
+    WINZIP="/cygdrive/c/Program\ Files/WinZip/wzzip"
+fi
 
-if ( $1 == "" ) then 
+if [ $1 == "" ] ; then 
     echo "Usage: createPackage <version> (e.g 3.X.x)"
-else 
-    echo Creating VirtuaWin package $1
+    exit -1
+fi
 
-    mkdir ./$1
-    cd ./$1
-    source ~/.cvssettings
+echo Creating VirtuaWin package $1
 
-    cvs checkout .
+mkdir ./$1
+cd ./$1
 
-    echo "Label repository? [y/n]"
-    if ( $< == 'y' ) then
-        setenv LABEL `echo V$1 | sed s/'\.'/_/g`
-        echo Labling with: $LABEL
-        cvs tag -R -F $LABEL
-    endif
+cvs checkout .
 
-    EDITOR Defines.h
-    echo "Start compilation? [y/n]"
-    if ( $< == 'y' ) then
-        make
-        cd WinList
-        make
-        cd ..
-        cd Modules/Assigner/
-        make
-        cd ../../..
-        echo done compiling!
-    endif
+read -p "Label repository? [y/n] " -n 1
+echo
+if [ $REPLY == 'y' ] ; then
+    export LABEL=`echo V$1 | sed s/'\.'/_/g`
+    echo Labelling with: $LABEL
+    cvs tag -R -F $LABEL
+fi
 
-    mkdir tmp
+mkdir tmp
+mkdir tmp/standard
+mkdir tmp/unicode
 
-    cp ./$1/VirtuaWin.exe ./tmp/
-    cp ./$1/WinList/WinList.exe ./tmp/
-    cp ./$1/Modules/Assigner/VWAssigner.exe ./tmp/
-    cp ./$1/READMEII.TXT ./tmp/README.TXT
-    cp ./$1/HISTORY.TXT ./tmp/
-    cp ./$1/COPYING.TXT ./tmp/
-    cp ./$1/userlist.cfg ./tmp/
-    cp ./$1/tricky.cfg ./tmp/
-    cp ./$1/Help/virtuawin.* ./tmp/
-    cp ./$1/scripts/virtuawin.iss ./tmp/
-    cp ./$1/scripts/VirtuaWin5.0.ISL ./tmp/
-    cp ./$1/scripts/filelist ./tmp/
-    
+$EDITOR Defines.h
+read -p "Compile source? [y/n] " -n 1
+echo
+if [ $REPLY == 'y' ] ; then
     echo compiling helpfile
-    cd ./tmp/
-    HELPCOMPILER virtuawin.hpj
-    mv VIRTUAWIN.HLP VirtuaWin.hlp
-    echo done!
-    
-    EDITOR virtuawin.iss
-
-    echo "Compile setup package? [y/n]"
-    if ( $< == 'y' ) then
-        SETUPCOMPILER virtuawin.iss
-        echo done!
-    endif
-
-    echo "Assemble source package? [y/n]"
-    if ( $< == 'y' ) then
-        echo Creating source package
-        cd ../$1
-        WINZIP source$1.zip -P @../tmp/filelist
-        echo Done!
-    endif
-
+    cd Help
+    $HELPCOMPILER /C /E virtuawin.hpj
     cd ..
+    echo building standard
+    ./build -S
+    ./build
+    cd WinList
+    ./build -S
+    ./build
+    cd ..
+    cd Modules/Assigner/
+    make
+    cd ../..
+    echo copying standard
+    cp ./VirtuaWin.exe ./tmp/standard/
+    cp ./WinList/WinList.exe ./tmp/standard/
+    cp ./Modules/Assigner/VWAssigner.exe ./tmp/standard/
+    cp ./READMEII.TXT ./tmp/standard/README.TXT
+    cp ./HISTORY.TXT ./tmp/standard/
+    cp ./COPYING.TXT ./tmp/standard/
+    cp ./userlist.cfg ./tmp/standard/
+    cp ./tricky.cfg ./tmp/standard/
+    cp ./Help/VirtuaWin.hlp ./tmp/standard/VirtuaWin.hlp
+    cp ./scripts/virtuawin.iss ./tmp/standard/
+    cp ./scripts/VirtuaWin5.0.ISL ./tmp/standard/
+    echo done standard
+    echo building unicode
+    ./build -S
+    ./build -u
+    cd WinList
+    ./build -S
+    ./build -u
+    cd ..
+    echo copying unicode
+    cp ./VirtuaWin.exe ./tmp/unicode/
+    cp ./WinList/WinList.exe ./tmp/unicode/
+    cp ./Modules/Assigner/VWAssigner.exe ./tmp/unicode/
+    cp ./READMEII.TXT ./tmp/unicode/README.TXT
+    cp ./HISTORY.TXT ./tmp/unicode/
+    cp ./COPYING.TXT ./tmp/unicode/
+    cp ./userlist.cfg ./tmp/unicode/
+    cp ./tricky.cfg ./tmp/unicode/
+    cp ./Help/VirtuaWin.hlp ./tmp/unicode/VirtuaWin.hlp
+    cp ./scripts/virtuawin.iss ./tmp/unicode/
+    cp ./scripts/VirtuaWin5.0.ISL ./tmp/unicode/
+    echo done unicode
+fi
 
-    echo "Move packages? [y/n]"
-    if ( $< == 'y' ) then
-        mkdir ./Distribution >& /dev/null
-        mv ./tmp/output/setup.exe ./Distribution/vwsetup$1.exe
-        mv ./$1/source$1.zip ./Distribution/
-    endif
+cd ./tmp/standard
 
-    echo "Upload to SourceForge? [y/n]"
-    if ( $< == 'y' ) then
-        ncftpput -d ./ftpsession.log -u anonymous -p virtuawin@home.se upload.sourceforge.net /incoming ./Distribution/vwsetup$1.exe
-        ncftpput -d ./ftpsession.log -u anonymous -p virtuawin@home.se upload.sourceforge.net /incoming ./Distribution/source$1.zip
-        echo Done! Go to SourceForge and click Admin-Edit/Release Files-Add Release and then type $1 and follow the instructions.
-    endif
+$EDITOR virtuawin.iss
 
+read -p "Compile standard setup package? [y/n] " -n 1
+echo
+if [ $REPLY == 'y' ] ; then
+    "$SETUPCOMPILER" /cc virtuawin.iss
+    echo done!
+fi
+
+cd ../unicode
+
+$EDITOR virtuawin.iss
+
+read -p "Compile unicode setup package? [y/n] " -n 1
+echo
+if [ $REPLY == 'y' ] ; then
+    "$SETUPCOMPILER" /cc virtuawin.iss
+    echo done!
+fi
+
+cd ../..
+
+read -p "Assemble source package? [y/n] " -n 1
+echo
+if [ $REPLY == 'y' ] ; then
+    echo Creating source package
+    rm -f VirtuaWin_source_$1.zip
+    if [ -z "$ZIP" ] ; then
+        $WINZIP VirtuaWin_source_$1.zip -P @./scripts/filelist
+    else        
+        $ZIP -@ < ./scripts/filelist
+        mv zip.zip VirtuaWin_source_$1.zip
+    fi
+    echo Done!
+fi
+
+read -p "Move packages? [y/n] " -n 1
+echo
+if [ $REPLY == 'y' ] ; then
+    mkdir ../Distribution >& /dev/null
+    mv ./tmp/standard/output/setup.exe ../Distribution/VirtuaWin_setup_$1.exe
+    mv ./tmp/unicode/output/setup.exe ../Distribution/VirtuaWin_unicode_setup_$1.exe
+    mv ./VirtuaWin_source_$1.zip ../Distribution/
+fi
+
+read -p "Upload to SourceForge? [y/n] " -n 1
+echo
+if [ $REPLY == 'y' ] ; then
+    ncftpput -d ./ftpsession.log -u anonymous -p virtuawin@home.se upload.sourceforge.net /incoming ../Distribution/VirtuaWin_setup_$1.exe
+    ncftpput -d ./ftpsession.log -u anonymous -p virtuawin@home.se upload.sourceforge.net /incoming ../Distribution/VirtuaWin_unicode_setup_$1.exe
+    ncftpput -d ./ftpsession.log -u anonymous -p virtuawin@home.se upload.sourceforge.net /incoming ../Distribution/VirtuaWin_source_$1.zip
+    echo Done! Go to SourceForge and click Admin-Edit/Release Files-Add Release and then type $1 and follow the instructions.
+fi
+
+read -p "Cleanup temporary files? [y/n] " -n 1
+echo
+if [ $REPLY == 'y' ] ; then
     echo cleanup temporary files
     rm -r ./tmp
     echo done!
 
-    echo "Delete $1 directory? [y/n]"
-    if  ( $< == 'y' ) then
+    cd ..
+
+    read -p "Delete $1 directory? [y/n] " -n 1
+    echo
+    if [ $REPLY == 'y' ] ; then
         rm -r ./$1
-    endif
+    fi
+fi
 
-    echo Packages created in "Distribution"!
-endif
+echo Packages created in "Distribution"!
 
-#
-# $Log$
-# Revision 1.6  2006/06/22 22:17:49  jopi
-# Updated with description and variables
-#
-# Revision 1.5  2006/06/09 18:06:13  jopi
-# Updated packaging scripts
-#
-# Revision 1.4  2006/04/05 15:10:31  jopi
-# version info moved
-#
-# Revision 1.3  2005/03/15 07:09:30  jopi
-# Minor update
-#
-# Revision 1.2  2005/03/15 06:58:22  jopi
-# Minor update
-#
-#
