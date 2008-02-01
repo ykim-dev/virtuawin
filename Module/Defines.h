a2f298b1f6bfb36fe57fc74e693a8fe5ca9052e7
bjasspa <>
2008-02-01 00:57:15 +0000

    Changed to V4.0 beta1

--- Module/Defines.h
+++ Module/Defines.h
@@ -1,4 +1,4 @@
-//
+/*
 //  VirtuaWin - Virtual Desktop Manager (virtuawin.sourceforge.net)
 //  ConfigParameters.h - Constant definitions used.
 // 
@@ -19,83 +19,157 @@
 //  along with this program; if not, write to the Free Software
 //  Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, 
 //  USA.
-//
+*/
 
 #ifndef _DEFINES_H_
 #define _DEFINES_H
 
 #include <windows.h>
 
-// application name and version defines
+/* Application name and version defines */
 #define vwVIRTUAWIN_NAME         _T("VirtuaWin")
+#define vwVIRTUAWIN_CLASSNAME    _T("VirtuaWinMainClass")
 #define vwVIRTUAWIN_EMAIL        _T("VirtuaWin@home.se")
-#define vwVIRTUAWIN_NAME_VERSION _T("VirtuaWin v3.2")
+#define vwVIRTUAWIN_NAME_VERSION _T("VirtuaWin v4.0 beta1")
+#define vwVIRTUAWIN_WEBSITE      _T("http://virtuawin.sourceforge.net/")
 
-// Various defines used on several places 
-#define vwWINDOW_MAX     160       // max number of windows to handle
-#define vwDESKTOP_MAX     20       // max number of desktops
+/* Various defines used on several places */
+#define vwHOTKEY_MAX      40       /* max number of hotkeys */
+#define vwWINHASH_SIZE   509       /* size of the window hash table */
+#define vwDESKTOP_MAX     20       /* max number of desktops */
 #define vwDESKTOP_SIZE    (vwDESKTOP_MAX + 2)
-#define vwCLASSNAME_MAX   64       // class name buffer size
-#define vwWINDOWNAME_MAX 128       // window name buffer size
-#define vwMODULENAME_MAX  79       // Maximum length of a module name (buffer needs to be n+1 long)
+#define vwCLASSNAME_MAX   64       /* class name buffer size */
+#define vwWINDOWNAME_MAX 128       /* window name buffer size */
+#define vwMODULENAME_MAX  79       /* Maximum length of a module name (buffer needs to be n+1 long) */
 
-#define MAXMODULES        10       // max number of modules to handle
+#define MAXMODULES        10       /* max number of modules to handle */
 
-// Internal windows messages
-#define VW_SYSTRAY        (WM_USER + 1)  // Sent to us by the systray
-#define VW_MOUSEWARP      (WM_USER + 9)  // Mouse thread message
+/* Internal windows messages */
+#define VW_SYSTRAY        (WM_USER + 1)  /* Sent to us by the systray */
+#define VW_MOUSEWARP      (WM_USER + 9)  /* Mouse thread message */
 
-#define vwTRICKY_POSITION 1
-#define vwTRICKY_WINDOW   2
+/* Some base types used to make code more readable */
+typedef unsigned int   vwUInt ;
+typedef unsigned short vwUShort ;
+typedef unsigned char  vwUByte ;
 
-#define vwVISIBLE_NO      0
-#define vwVISIBLE_YES     1
-#define vwVISIBLE_TEMP    2
-#define vwVISIBLE_YESTEMP 3
 
-#define vwWTFLAGS_NO_TASKBAR_BUT  0x01
-#define vwWTFLAGS_RM_TASKBAR_BUT  0x02
+#define vwWTNAME_COUNT            3
+#define vwWTFLAGS_CN_SVAR         0x00000001
+#define vwWTFLAGS_CN_EVAR         0x00000002
+#define vwWTFLAGS_WN_SVAR         0x00000004
+#define vwWTFLAGS_WN_EVAR         0x00000008
+#define vwWTFLAGS_PN_SVAR         0x00000010
+#define vwWTFLAGS_PN_EVAR         0x00000020
+#define vwWTFLAGS_STICKY          0x00001000
+#define vwWTFLAGS_MOVE            0x00002000
+#define vwWTFLAGS_MOVE_IMMEDIATE  0x00004000
+#define vwWTFLAGS_HIDEWIN_MASK    0x000f0000
+#define vwWTFLAGS_HIDEWIN_BITROT  16
+#define vwWTFLAGS_HIDEWIN_HIDE    0x00000000
+#define vwWTFLAGS_HIDEWIN_MOVE    0x00010000
+#define vwWTFLAGS_HIDEWIN_MINIM   0x00020000
+#define vwWTFLAGS_HIDETSK_MASK    0x00f00000
+#define vwWTFLAGS_HIDETSK_BITROT  20
+#define vwWTFLAGS_HIDETSK_HIDE    0x00000000
+#define vwWTFLAGS_HIDETSK_DONT    0x00100000
+#define vwWTFLAGS_HIDETSK_TOOLWN  0x00200000
+#define vwWTFLAGS_MANAGE          0x20000000
+#define vwWTFLAGS_DONT_MANAGE     0x40000000
+#define vwWTFLAGS_ENABLED         0x80000000
 
-typedef struct { // Holds the windows in the list
-    HWND           Handle;
-    HWND           Owner;
-    long           Style;
-    long           ExStyle;
-    unsigned long  ZOrder[vwDESKTOP_SIZE] ;
-    unsigned short Desk;
-    unsigned short menuId ;
-    unsigned char  Sticky;
-    unsigned char  Tricky;
-    unsigned char  Visible;
-    unsigned char  State;
-    unsigned char  Flags;
-} windowType;
+typedef struct vwWindowType {
+    struct vwWindowType *next;
+    TCHAR               *name[vwWTNAME_COUNT] ;
+    vwUInt               flags ; 
+    vwUByte              desk ;
+    vwUByte              nameLen[vwWTNAME_COUNT] ;
+} vwWindowType ;
 
-// Holds data for modules
+/* vwWindow structures for storing information about one window */
+#define vwWINFLAGS_INITIALIZED     0x00000001
+#define vwWINFLAGS_FOUND           0x00000002
+#define vwWINFLAGS_ACTIVATED       0x00000004
+#define vwWINFLAGS_VISIBLE         0x00000008
+#define vwWINFLAGS_MINIMIZED       0x00000010
+#define vwWINFLAGS_WINDOW          0x00000020
+#define vwWINFLAGS_MANAGED         0x00000040
+#define vwWINFLAGS_TRICKY_POS      0x00000080
+#define vwWINFLAGS_SHOWN           0x00000100
+#define vwWINFLAGS_SHOW            0x00000200
+#define vwWINFLAGS_NO_TASKBAR_BUT  0x00000400
+#define vwWINFLAGS_RM_TASKBAR_BUT  0x00000800
+#define vwWINFLAGS_STICKY          vwWTFLAGS_STICKY        
+#define vwWINFLAGS_MOVE            vwWTFLAGS_MOVE          
+#define vwWINFLAGS_MOVE_IMMEDIATE  vwWTFLAGS_MOVE_IMMEDIATE
+#define vwWINFLAGS_MAXIMIZED       0x00008000
+#define vwWINFLAGS_HIDEWIN_MASK    vwWTFLAGS_HIDEWIN_MASK  
+#define vwWINFLAGS_HIDEWIN_HIDE    vwWTFLAGS_HIDEWIN_HIDE  
+#define vwWINFLAGS_HIDEWIN_MOVE    vwWTFLAGS_HIDEWIN_MOVE  
+#define vwWINFLAGS_HIDEWIN_MINIM   vwWTFLAGS_HIDEWIN_MINIM 
+#define vwWINFLAGS_HIDETSK_MASK    vwWTFLAGS_HIDETSK_MASK  
+#define vwWINFLAGS_HIDETSK_HIDE    vwWTFLAGS_HIDETSK_HIDE  
+#define vwWINFLAGS_HIDETSK_DONT    vwWTFLAGS_HIDETSK_DONT  
+#define vwWINFLAGS_HIDETSK_TOOLWN  vwWTFLAGS_HIDETSK_TOOLWN
+
+/* vwWindowBase - Holds data far a non-managed window */
+typedef struct vwWindowBase { 
+    struct vwWindowBase *next ;
+    struct vwWindowBase *hash ;
+    HWND                 handle ;
+    vwUInt               flags ;
+} vwWindowBase;
+
+/* vwWindow - Holds data far a managed window, start must be the same as vwWindowBase */
+typedef struct vwWindow { 
+    /* same as vwWindowBase - start */
+    struct vwWindow     *next ;
+    struct vwWindow     *hash ;
+    HWND                 handle ;
+    vwUInt               flags ;
+    /* same as vwWindowBase - end */
+    HWND                 owner;
+    long                 exStyle;
+    vwUInt               zOrder[vwDESKTOP_SIZE] ;
+    vwUByte              menuId ;
+    vwUByte              desk;
+} vwWindow ;
+
+/* Holds data for modules */
 typedef struct {
-    HWND   Handle;
-    BOOL   Disabled;
-    TCHAR  description[vwMODULENAME_MAX+1];
+    HWND      handle;
+    TCHAR     description[vwMODULENAME_MAX+1];
+    vwUByte   disabled;
 } moduleType;
 
-// Holds disabled modules
+/* Holds disabled modules */
 typedef struct {
-    TCHAR  moduleName[vwMODULENAME_MAX+1];
+    TCHAR     moduleName[vwMODULENAME_MAX+1];
 } disModules;
 
+/* vwMenuItem - Structure used by the window list menu */
 typedef struct {
-    TCHAR         *name;
-    HICON          icon; 
-    unsigned short id;
-    unsigned short desk;
-    unsigned char  sticky;
+    TCHAR    *name;
+    HICON     icon; 
+    vwUInt    zOrder ;
+    vwUShort  id;
+    vwUByte   desk;
+    vwUByte   sticky;
 } vwMenuItem ;
 
-typedef struct vwWindowMatch {
-    struct vwWindowMatch *next;
-    TCHAR                *match;
-    unsigned short       desk;
-    unsigned char        type;
-} vwWindowMatch ;
+/* vwHotkey - Structure to store a hotkey binding */
+#define vwHOTKEY_ALT     MOD_ALT
+#define vwHOTKEY_CONTROL MOD_CONTROL
+#define vwHOTKEY_SHIFT   MOD_SHIFT
+#define vwHOTKEY_WIN     MOD_WIN
+#define vwHOTKEY_EXT     0x10
+
+typedef struct {
+    ATOM     atom ;
+    vwUByte  key ;
+    vwUByte  modifier ;
+    vwUByte  command ;
+    vwUByte  desk ;
+} vwHotkey ;
 
 #endif
