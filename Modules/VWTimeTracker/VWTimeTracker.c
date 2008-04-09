8210ef2e20dace28e52aaeeb755d1da65c6b8a6b
bjasspa <>
2008-04-09 11:17:41 +0000

    Up-dated copyright to 2008

--- Modules/VWTimeTracker/VWTimeTracker.c
+++ Modules/VWTimeTracker/VWTimeTracker.c
@@ -2,7 +2,7 @@
  *  VirtuaWin - Virtual Desktop Manager (virtuawin.sourceforge.net)
  *  VWTimeTracker.c - Module to display the time spent on each desk.
  * 
- *  Copyright (c) 2006-2007 VirtuaWin (VirtuaWin@home.se)
+ *  Copyright (c) 2006-2008 VirtuaWin (VirtuaWin@home.se)
  * 
  *  This program is free software; you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License as published by
